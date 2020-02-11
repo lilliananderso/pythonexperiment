c3986874136a6e0b96accbc89f4f52fdff5b6ed9
lilliananderso <57363174+lilliananderso@users.noreply.github.com>
2020-02-11 10:44:24 -0600

    Commit Message

---
+++ 3_3_5_Lillian_Henry.py
@@ -0,0 +1,13 @@
+slogan = 'My school is the best'
+
+print(slogan[17:21])
+print(slogan[:13] + 'awesome!')
+
+activity = 'theater'
+print(len(activity))
+
+phrase = 'Greatest good for the greatest number!'
+print('test goo' in phrase)
+
+test = 'How many characters are in this sentence?'
+print(len(test))
